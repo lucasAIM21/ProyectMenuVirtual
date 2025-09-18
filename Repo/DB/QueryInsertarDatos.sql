b7f9042f66636d9992109c0a980a11196490b51f
Ubuntu <ubuntu@ip-172-31-35-36.ec2.internal>
2025-09-18 15:51:17 +0000

    Agregue datos a la BD

--- Repo/DB/QueryInsertarDatos.sql
+++ Repo/DB/QueryInsertarDatos.sql
@@ -1,1 +1,12 @@
+USE DB_Menu;
 
+INSERT INTO Imagenes(RutaImagen,ProductoID) VALUES
+	('/imgs/Chaufa.jpg',NULL),('/imgs/ArrozConPollo.jpeg',NULL),('/imgs/PolloALaBrasa.jpg',NULL);
+INSERT INTO Categoria(nombre,ImagenID) VALUES 
+	('Plato Fuerte',3);
+INSERT INTO Productos(nombre,Precio,Ingredientes,CategoriaID) VALUES
+	('Chaufa',12.00,'Arroz, pollo, cebolla  china, sillao',1),
+	('Arroz con pollo',10.00,'Arroz, verduras, pollo, papas, crema',1);
+UPDATE Imagenes SET ProductoID = 1 WHERE ImagenID = 1;
+
+UPDATE Imagenes SET ProductoID = 2 WHERE ImagenID = 2;
