d2be30cc07f171ccf88ad85c1e4b064fbee08acb
Ubuntu <ubuntu@ip-172-31-35-36.ec2.internal>
2025-09-19 03:51:39 +0000

    asdqw

--- Repo/Backend/config/db.js
+++ Repo/Backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "Liam7687.",
+	password: "Liam_7687",
 	database: "DB_Menu"
 });
 
