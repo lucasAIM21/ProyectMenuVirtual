24bb5ff767553e22bab77d728f49144c4d66835d
Lucas Ingaruca <lucaspe123321@gmail.com>
2025-09-18 22:22:52 -0500

    gawe

--- Repo/Backend/routes/productos.js
+++ Repo/Backend/routes/productos.js
@@ -3,14 +3,23 @@ const router = express.Router();
 const db = require("../config/db");
 
 router.get("/",(req,res) => {
+
+	const query= `
+		select
+			p.ProductoID as id,
+			p.combre,
+			p.Precio as precio,
+			P.Ingredientes as descripcion,
+			i.RutaImagen as imagen
+		from productos p
+		left join imagenes i on p.ProductoID=i.ProductoID	
+	`;
+
 	db.query("SELECT * FROM Productos",(err,results) => {
 		if(err){
 			return res.status(500).json({error: "Error en la consulta"});
 		}
-		const productos=results.map(producto => ({
-			...producto,
-			imagen: `/imgs/${producto.imagen}`
-		}));
+		
 	res.json(productos);
 	});
 });
