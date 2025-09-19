245501e5c0846e56cffffd9f381c2b66be59161f
Lucas Ingaruca <lucaspe123321@gmail.com>
2025-09-18 19:58:00 -0500

    aaa

---
+++ Repo/Front/GetProductos.js
@@ -0,0 +1,28 @@
+async function CargarMenu() {
+    try{
+        const response = await fetch("http://localhost:3000/productos");
+        const productos = await response.json();
+
+        const menu = document.getElementById("menu");
+        menu.innerHTML="";
+
+        productos.forEach(p => {
+            const item = document.createElement("div");
+            item.classList.add("platillo");
+
+            item.innerHTML= `
+                <img src="/imgs/${p.imagen}" alt="${p.nombre}">
+                <div class = "platillo-info">
+                <h2>${p.nombre}</h2>
+                <p>${p.descripcion}<p>
+                <p class="precio">S/.${p.precio}<p>
+                </div>
+            `;
+            menu.appendChild(item);
+        });
+    }catch (err) {
+        console.error("Error al cargar menu: ",err);
+    }
+}
+
+CargarMenu();
