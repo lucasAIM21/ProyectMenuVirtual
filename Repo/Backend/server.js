da0db2b7211817686302100a344f963502ec8323
Lucas Ingaruca <lucaspe123321@gmail.com>
2025-09-23 14:19:01 -0500

    xcyu

--- Repo/Backend/server.js
+++ Repo/Backend/server.js
@@ -10,6 +10,9 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, "../Front")));
 
 // Ruta de prueba simple
