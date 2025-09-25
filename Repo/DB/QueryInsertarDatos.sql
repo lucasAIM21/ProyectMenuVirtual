a2bbd5982233ff71ff63e030f6fc7bb5f00d1dd1
Lucas Ingaruca <lucaspe123321@gmail.com>
2025-09-25 00:48:48 -0500

    editando DB

--- Repo/DB/QueryInsertarDatos.sql
+++ Repo/DB/QueryInsertarDatos.sql
@@ -3,14 +3,16 @@ USE DB_Menu;
 INSERT INTO ImgProductos(RutaImagen,ProductoId) VALUES
 	('/imgs/imgP/Chaufa.jpg',NULL),('/imgs/imgP/ArrozConPollo.jpeg',NULL),('/imgs/imgP/PolloALaBrasa.jpg',NULL);
 INSERT INTO ImgCategorias(RutaImagen) VALUES
-	('/imgs/imgC/PlatoFuerte.png',NULL);
+	('/imgs/imgC/PlatoFuerte.png');
 INSERT INTO Categoria(nombre,ImgId) VALUES 
 	('Plato Fuerte',1);
 INSERT INTO Productos(nombre,Precio,Ingredientes,CategoriaId) VALUES
 	('Chaufa',12.00,'Arroz, pollo, cebolla  china, sillao',1),
-	('Arroz con pollo',10.00,'Arroz, verduras, pollo, papas, crema',1);
+	('Arroz con pollo',10.00,'Arroz, verduras, pollo, papas, crema',1),
 	('Pollo a la brasa',15.00,'Pollo, papas fritas, ensalada',1);
-	
-UPDATE ImgProductos SET ProductoID = 1 WHERE ImagenID = 1;
 
-UPDATE ImgProductos SET ProductoID = 2 WHERE ImagenID = 2;
+UPDATE ImgProductos SET ProductoId = 1 WHERE ImgId = 1;
+
+UPDATE ImgProductos SET ProductoId = 2 WHERE ImgId = 2;
+
+UPDATE ImgProductos SET ProductoId = 3 WHERE ImgId = 3;
