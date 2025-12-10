264399eb273f0ee1bcb728ccede89afbe8aa6f1a
Lucas Ingaruca <lucaspe123321@gmail.com>
2025-12-09 19:25:43 -0500

    aea

--- Repo/Backend/config/multer.js
+++ Repo/Backend/config/multer.js
@@ -1,4 +1,7 @@
 // 📂 Configuración de multer
+const multer = require('multer');
+const path = require('path');
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, path.join(__dirname, "../../imgs/imgP"));
