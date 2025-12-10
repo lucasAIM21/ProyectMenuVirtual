9cd2ce296ee52ac94fadc579f9ddaac2b176bc8d
Lucas Ingaruca <lucaspe123321@gmail.com>
2025-12-10 14:09:09 -0500

    aea

--- Repo/Backend/config/multer.js
+++ Repo/Backend/config/multer.js
@@ -4,9 +4,15 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "../../imgs/imgP"));
+        cb(null, path.join(__dirname, "../imgs/imgP"));
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
     }
 });
+
+// 👉 Crear instancia de multer con este storage
+const upload = multer({ storage });
+
+// 👉 Exportar correctamente
+module.exports = upload;
