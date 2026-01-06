9168995306cc9c90158d9228140174132c8c224c
Lucas Ingaruca <lucaspe123321@gmail.com>
2026-01-05 23:17:40 -0500

    aea

--- Repo/DB/DbSoloProductos.sql
+++ Repo/DB/DbSoloProductos.sql
@@ -1,17 +1,3 @@
-
-DROP TABLE IF EXISTS `Categoria`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Categoria` (
-  `CategoriaId` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) NOT NULL,
-  `ImgId` int DEFAULT NULL,
-  PRIMARY KEY (`CategoriaId`),
-  KEY `ImgId` (`ImgId`),
-  CONSTRAINT `Categoria_ibfk_1` FOREIGN KEY (`ImgId`) REFERENCES `ImgCategorias` (`ImgId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Table structure for table `ImgCategorias`
 --
@@ -26,21 +12,17 @@ CREATE TABLE `ImgCategorias` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `ImgProductos`
---
-
-DROP TABLE IF EXISTS `ImgProductos`;
+DROP TABLE IF EXISTS `Categoria`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ImgProductos` (
-  `ImgId` int NOT NULL AUTO_INCREMENT,
-  `RutaImagen` varchar(255) NOT NULL,
-  `ProductoId` int DEFAULT NULL,
-  PRIMARY KEY (`ImgId`),
-  KEY `FK_ImgProductos_Productos` (`ProductoId`),
-  CONSTRAINT `FK_ImgProductos_Productos` FOREIGN KEY (`ProductoId`) REFERENCES `Productos` (`ProductoId`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `Categoria` (
+  `CategoriaId` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `ImgId` int DEFAULT NULL,
+  PRIMARY KEY (`CategoriaId`),
+  KEY `ImgId` (`ImgId`),
+  CONSTRAINT `Categoria_ibfk_1` FOREIGN KEY (`ImgId`) REFERENCES `ImgCategorias` (`ImgId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,4 +43,22 @@ CREATE TABLE `Productos` (
   KEY `CategoriaId` (`CategoriaId`),
   CONSTRAINT `Productos_ibfk_1` FOREIGN KEY (`CategoriaId`) REFERENCES `Categoria` (`CategoriaId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Table structure for table `ImgProductos`
+--
+
+DROP TABLE IF EXISTS `ImgProductos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ImgProductos` (
+  `ImgId` int NOT NULL AUTO_INCREMENT,
+  `RutaImagen` varchar(255) NOT NULL,
+  `ProductoId` int DEFAULT NULL,
+  PRIMARY KEY (`ImgId`),
+  KEY `FK_ImgProductos_Productos` (`ProductoId`),
+  CONSTRAINT `FK_ImgProductos_Productos` FOREIGN KEY (`ProductoId`) REFERENCES `Productos` (`ProductoId`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 
