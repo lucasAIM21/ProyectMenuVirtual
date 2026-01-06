eb252550b81ab897f4100d078d51d82634aa611f
Lucas Ingaruca <lucaspe123321@gmail.com>
2026-01-05 23:35:38 -0500

    aea

--- Repo/DB/AgregarCategoria.sql
+++ Repo/DB/AgregarCategoria.sql
@@ -1,4 +1,4 @@
 INSERT INTO ImgCategorias(RutaImagen) VALUES
 	('/imgs/imgC/PlatoFuerte.png');
 INSERT INTO Categoria(nombre,ImgId) VALUES 
-	('Plato Fuerte',1);
+	('Plato Fuerte',LAST_INSERT_ID());
