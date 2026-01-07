01a5c798ccb36e87c93b715e4b2526785dec47ac
Lucas Ingaruca <lucaspe123321@gmail.com>
2026-01-06 23:02:39 -0500

    aea

--- Repo/DB/PedidosDePrueba.sql
+++ Repo/DB/PedidosDePrueba.sql
@@ -1,5 +1,5 @@
 INSERT INTO PEDIDOS (NroMesa, Monto, EstadoId, TipoPagoId)
-VALUES (3, 24.00, 1, 1)
+VALUES (3, 24.00, 1, 1);
 
 INSERT INTO PRODUCTOS_DEL_PEDIDO (PedidoId, ProductoId, Cantidad, PrecioUnitario)
 VALUES (1, 2, 3, 10.00), (1, 1, 1, 12.00);
