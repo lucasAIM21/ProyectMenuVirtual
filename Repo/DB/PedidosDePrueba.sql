4671c929ac367c2c1db8b2c68741ab07e6c748f6
Lucas Ingaruca <lucaspe123321@gmail.com>
2026-01-06 23:09:12 -0500

    aea

--- Repo/DB/PedidosDePrueba.sql
+++ Repo/DB/PedidosDePrueba.sql
@@ -4,4 +4,4 @@ VALUES (3, 24.00, 1, 1);
 SET @PedidoId = LAST_INSERT_ID();
 
 INSERT INTO PRODUCTOS_DEL_PEDIDO (PedidoId, ProductoId, Cantidad, PrecioUnitario)
-VALUES (@PedidoId, 2, 3, 10.00), (@PedidoId, 1, 1, 12.00);
+VALUES (@PedidoId, 27, 3, 10.00), (@PedidoId, 26, 1, 12.00);
